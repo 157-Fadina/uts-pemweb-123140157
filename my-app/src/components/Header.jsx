b2944962efc11a25c1cf6bd3d6ed5fd5bdf97053
157-Fadina <fadina.123140157@student.itera.ac.id>
2025-11-02 21:07:57 +0700

    menambahkan logo

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
-import { 
-  Navbar, 
-  Container, 
-  Button,
-  Badge 
-} from 'react-bootstrap';
+import { Navbar, Container, Button, Badge } from 'react-bootstrap';
 
 function Header({ playlistCount, onShowPlaylist }) {
   return (
     <Navbar bg="#4a3c5c" data-bs-theme="dark" expand="lg" className="main-bar">
       <Container>
-        <Navbar.Brand href="/">VoxFinder</Navbar.Brand>
-          <Button variant="warning" onClick={onShowPlaylist}>
-            Playlist 
-            <Badge bg="dark" className="ms-1">{playlistCount}</Badge>
-          </Button>
+        <Navbar.Brand href="/" className="d-flex align-items-center">
+          <img
+            src="./src/assets/LOGO.png"
+            alt="Logo"
+            width="35"
+            height="35"
+            className="me-2"
+          />
+          <span>VoxFinder</span>
+        </Navbar.Brand>
+
+        <Button variant="warning" onClick={onShowPlaylist}>
+          Playlist
+          <Badge bg="dark" className="ms-1">{playlistCount}</Badge>
+        </Button>
       </Container>
     </Navbar>
   );
